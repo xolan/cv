4c5a72094ee09e05f513c95c030ae81ddc23cad2
Jonas Svarvaa <jonassvarvaa@gmail.com>
2012-09-25 03:53:23 -0700

    data

---
+++ src/data-example.js
@@ -0,0 +1,89 @@
+var data = {
+    cvURL: "the url to this web site",
+    firstName: "First",
+    lastName: "Last",
+    address: "Street address",
+    zip: "Zip",
+    city: "City",
+    country: "Country",
+    email: "email@example.com",
+    languages: [
+        "norwegian",
+        "english",
+        "german"
+    ],
+    programming: [
+        "Java, Android",
+        "Python, Django",
+        "PHP, Wordpress",
+        "HTML5, CSS3, JS"
+    ],
+    other: [
+        "norwegian driver's licence, class B",
+        "served mandatory military service"
+    ],
+    interests: [
+        "user interface programming",
+        "user interface design",
+        "game design",
+        "gaming",
+        "network programming",
+        "graphical design",
+        "music"
+    ],
+
+    education: [
+        {
+            when: "Summer 2009",
+            what: "NTNU, Bachelor in informatics",
+            where: "Norwegian University of Science and Technology"
+        }, {
+            when: "2007-2008",
+            what: "Sound Engineering",
+            where: "Trøndertun Folkehøyskole"
+        }, {
+            when: "2004-2005",
+            what: "Cambridge Young Enterprise International Examination",
+            where: ""
+        }
+    ],
+
+    experience: [
+        {
+            when: "Summer 2011",
+            what: "Insurgency Tour 2011<br/>28 concerts in 28 day, 7 cities every week, over 800km of driving<br/>Organizer, Management, Marketing, Web Design, Musician",
+            where: "Apeiron Restraint"
+        }, {
+            when: "Since 2009",
+            what: "Bass player, co-founder and co-writer",
+            where: "Apeiron Restraint"
+        }
+    ],
+
+    extra: [
+        {
+            when: "Since 2011",
+            what: "System administration and development<br/>Primarily using Python and Django",
+            where: "Online Student Union, NTNU"
+        }
+    ],
+
+    online: [
+        {
+            link: "http://github.com/xolan",
+            desc: "Collection of personal public open source repositories."
+        }, {
+            link: "http://online.ntnu.no",
+            desc: "Online Student Union's site. Technologies used include Python, Django, LDAP and others."
+        }, {
+            link: "http://studlan.no",
+            desc: "Dedicated site for Online's studLAN-group. It's made using Django, and the source is available at https://github.com/dotKom/studlan."
+        }, {
+            link: "http://apeironrestraint.com",
+            desc: "My band, Apeiron Restraint's web site. Made using PHP and Wordpress, with a custom theme."
+        }, {
+            link: "http://insurgencytour.com",
+            desc: "Insurgency Tour's web site. Written using PHP."
+        }, 
+    ]
+}
---
+++ src/data.js
@@ -0,0 +1,125 @@
+var data = {
+    cvURL: "https://github.com/xolan/cv",
+    firstName: "Jonas",
+    lastName: "Svarvaa",
+    address: "Njardarvollen 9",
+    zip: "7032",
+    city: "Trondheim",
+    country: "Norway",
+    email: "jonassvarvaa@gmail.com",
+    languages: [
+        "norwegian",
+        "english",
+        "german notions"
+    ],
+    programming: [
+        "Java, Android",
+        "Python, Django",
+        "PHP, Wordpress",
+        "HTML5, CSS3, JS"
+    ],
+    other: [
+        "norwegian driver's licence, class B",
+        "served mandatory military service"
+    ],
+    interests: [
+        "user interface programming",
+        "user interface design",
+        "game design",
+        "gaming",
+        "network programming",
+        "graphical design",
+        "music"
+    ],
+
+    education: [
+        {
+            when: "Summer 2009",
+            what: "NTNU, Bachelor in informatics",
+            where: "Norwegian University of Science and Technology"
+        }, {
+            when: "2007-2008",
+            what: "Sound Engineering",
+            where: "Trøndertun Folkehøyskole"
+        }, {
+            when: "2004-2005",
+            what: "Cambridge Young Enterprise International Examination",
+            where: ""
+        }, {
+            when: "2003-2006",
+            what: "Media and Communication",
+            where: "Levanger Videregående Skole"
+        }, {
+            when: "2000-2003",
+            what: "Junior High School",
+            where: "Frol Ungdomskole"
+        }, {
+            when: "1994-2003",
+            what: "Elementary School",
+            where: "Mule Oppvekstsenter"
+        }
+    ],
+
+    experience: [
+        {
+            when: "Summer 2011",
+            what: "Insurgency Tour 2011<br/>28 concerts in 28 day, 7 cities every week, over 800km of driving<br/>Organizer, Management, Marketing, Web Design, Musician",
+            where: "Apeiron Restraint"
+        }, {
+            when: "Since 2009",
+            what: "\"Guru\", helpdesk<br/>IT support for students and employees of the IME and IDI faculties",
+            where: "Gurutjenesten, IDI, NTNU"
+        }, {
+            when: "Since 2009",
+            what: "Bass player, co-founder and co-writer",
+            where: "Apeiron Restraint"
+        }, {
+            when: "2008-2009",
+            what: "Graphical and Web Designer",
+            where: "Lydhagen Studio AS, Verdal"
+        }, {
+            when: "2008",
+            what: "Farm hand",
+            where: "Sæter Gård, Levanger"
+        }
+    ],
+
+    extra: [
+        {
+            when: "Since 2011",
+            what: "System administration and development<br/>Primarily using Python and Django",
+            where: "Online Student Union, NTNU"
+        }, {
+            when: "2007-2008",
+            what: "Student Counsil Representative",
+            where: "Trøndertun Folkehøyskole"
+        }, {
+            when: "2003-2004",
+            what: "Student Counsil Representative",
+            where: "Levanger Videregående Skole"
+        }, {
+            when: "2002-2003",
+            what: "Student Counsil Representative",
+            where: "Frol Ungdomskole"
+        }
+    ],
+
+    online: [
+        {
+            link: "http://github.com/xolan",
+            desc: "Collection of personal public open source repositories."
+        }, {
+            link: "http://online.ntnu.no",
+            desc: "Online Student Union's site. Technologies used include Python, Django, LDAP and others."
+        }, {
+            link: "http://studlan.no",
+            desc: "Dedicated site for Online's studLAN-group. It's made using Django, and the source is available at https://github.com/dotKom/studlan."
+        }, {
+            link: "http://apeironrestraint.com",
+            desc: "My band, Apeiron Restraint's web site. Made using PHP and Wordpress, with a custom theme."
+        }, {
+            link: "http://insurgencytour.com",
+            desc: "Insurgency Tour's web site. Written using PHP."
+        }, 
+    ]
+}
